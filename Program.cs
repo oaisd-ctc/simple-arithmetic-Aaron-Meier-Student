c3fd2dc4d15cfa2a9eccddaa0672ec95cca672b9
Aaron-Meier-Student <aamea72@student.oaisd.org>
2024-10-14 13:05:17 +0000

    Created C# file with code that evaluates 2 inputs with a choosen operator.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("What is X?");
+        float x = float.Parse(Console.ReadLine());
+        Console.WriteLine("What is Y?");
+        float y = float.Parse(Console.ReadLine());
+        Console.WriteLine("What opperation do you want to do? + - * / %");
+        string opperator = Console.ReadLine();
+        double result = 0;
+        if (opperator == "+") {
+            result = x+y;
+        } else if (opperator == "-") {
+            result = x-y;
+        } else if (opperator == "*") {
+            result = x*y;
+        } else if (opperator == "/") {
+            result = x/y;
+        } else if (opperator == "%") {
+            result = x%y;
+        }
+        Console.WriteLine(result);
+    }
+}
